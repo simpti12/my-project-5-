102de21c11534e594860fffbc94033d4d0ec2f3a
simpti12 <125181474+simpti12@users.noreply.github.com>
2023-02-12 11:19:21 +0300

    HeloWorld

---
+++ HeloWorld.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("HeloWorld");
+    } 
+    // Update is called once per 
+    void Update()
+    {
+       
+    }
